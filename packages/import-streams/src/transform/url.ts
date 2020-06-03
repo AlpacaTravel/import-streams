314ed14d1350c31bbc94c812e38530a0bc0d86f6
Cam Manderson <cam@alpaca.travel>
2020-06-03 20:05:44 +1000

    Adds option to lowercaseHostname to url

--- packages/import-streams/src/transform/url.ts
+++ packages/import-streams/src/transform/url.ts
@@ -1,15 +1,17 @@
 import { TransformFunction, TransformOptions } from "../types";
 import { assertValidTransformOptions } from "../assertions";
+import URI from "urijs";
 
 export interface UrlOptions extends TransformOptions {
   prefix?: string;
+  lowercaseHostname?: boolean;
 }
 
 const url: TransformFunction<Promise<any>, UrlOptions> = async (
   value: any,
   options: UrlOptions
 ): Promise<string | undefined> => {
-  assertValidTransformOptions(options, ["prefix"], "url");
+  assertValidTransformOptions(options, ["prefix", "lowercaseHostname"], "url");
   if (typeof value === "string") {
     let newValue = value;
     if (options && options.prefix) {
@@ -20,10 +22,16 @@ const url: TransformFunction<Promise<any>, UrlOptions> = async (
       newValue = `http://${newValue}`;
     }
 
+    // Support the option to lowercase hostname
+    if (options.lowercaseHostname === true) {
+      const parsed = new URI(newValue);
+      return parsed.hostname(parsed.hostname().toLocaleLowerCase()).toString();
+    }
+
     return newValue;
   }
 
-  return value;
+  return undefined;
 };
 
 export default url;
---
+++ packages/import-streams/tests/src/transform/url.test.ts
@@ -0,0 +1,48 @@
+import { Readable, Writable } from "readable-stream";
+import url from "../../../src/transform/url";
+import Transforms from "../../../src/transform/index";
+
+import { createCompose, transforms } from "../../../src/index";
+import { ComposeContext } from "../../../src/types";
+
+const createdCompose = createCompose();
+
+const context: ComposeContext = {
+  compose: createdCompose,
+};
+
+describe("URL", () => {
+  test("exports", () => {
+    expect(typeof transforms["url"]).toBe("function");
+  });
+
+  test("a string", async () => {
+    expect(
+      await url("https://alpaca.travel/example", {
+        context,
+      })
+    ).toBe("https://alpaca.travel/example");
+
+    expect(
+      await url("alpaca.travel/example", {
+        context,
+      })
+    ).toBe("http://alpaca.travel/example");
+
+    expect(
+      await url("https://alpaca.TRAVEL/example", {
+        lowercaseHostname: true,
+        context,
+      })
+    ).toBe("https://alpaca.travel/example");
+
+    expect(
+      await url(
+        { foo: "bar" },
+        {
+          context,
+        }
+      )
+    ).toBeUndefined();
+  });
+});
